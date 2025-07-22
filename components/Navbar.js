b131240be50eea10cec045122cf45f6010939690
Abbas Sihorwala <146643548+Abbas-Sihor@users.noreply.github.com>
2025-07-22 23:50:49 +0530

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -154,7 +154,7 @@ const Navbar = () => {
             type="text"
             value={query}
             onChange={(e) => setQuery(e.target.value)}
-            placeholder="Search username..."
+            placeholder="ex: abbassihorwala53"
             className="w-full mb-2 px-3 py-2 text-black border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:outline-none"
           />
           <button
